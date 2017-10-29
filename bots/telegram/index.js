d9c22fd7d2a2996e8b6f7a04c6abddc903785c9e
DevelPlusNUWM <DevelPlusNUWM@develplus.github.com>
2017-10-29 03:58:39 +0300

    4

--- bots/telegram/index.js
+++ bots/telegram/index.js
@@ -7,33 +7,30 @@ const TelegramBot = require('node-telegram-bot-api');
 var Telegram = (function() {
     
     const token = CONSTS.Config.get('Telegram.tokken')
-    const bot = new TelegramBot(token, {polling: { interval: 2000, timeout: 10 }});
-    //var res;
+    
+        
+    const bot = new TelegramBot(token, {
+        webHook: {
+            post: CONSTS.Config.get('port')
+        }
+    });
+    
 
-    bot.onText(/(.+)/, (msg, match) => {
-        // 'msg' is the received Message from Telegram
-        // 'match' is the result of executing the regexp above on the text content
-        // of the message
-      
-
-         
+    bot.setWebHook(CONSTS.Config.get('url') + "/bot" + token);
+    
+    bot.on('message', msg => {
+        
         res = TBot.Bot.InputRes(msg)
         
         console.log(res);
         bot.sendMessage(res.fromId, res.respoynse);
-        console.log(res + "respounsed");
-        //const chatId = msg.chat.id;
-        //const resp = match[1]; // the captured "whatever"
+        //console.log(res + "respounsed");
+    
     });
-
-
-    //console.log(res);
-    //bot.sendMessage(res.fromId, res.respoynse);
-
-
+    
     return {
         Start: function() {
-            console.log("Telegram Bot sacsesful started");
+           // console.log("Telegram Bot sacsesful started");
         }
     }
 
