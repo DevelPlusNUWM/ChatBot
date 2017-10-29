11c84110a72a498eab9b4a22ea05b6468f932ebc
DevelPlusNUWM <DevelPlusNUWM@develplus.github.com>
2017-10-29 03:02:24 +0200

    Revert "123"
    This reverts commit 7b5a9225c250f0bceaf0dde1e6aa7108454ab6db.

--- bots/telegram/index.js
+++ bots/telegram/index.js
@@ -7,30 +7,22 @@ const TelegramBot = require('node-telegram-bot-api');
 var Telegram = (function() {
     
     const token = CONSTS.Config.get('Telegram.tokken')
-    
-        
-    const bot = new TelegramBot(token, {
-        webHook: {
-            post: CONSTS.Config.get('port')
-        }
-    });
-    
+    const bot = new TelegramBot(token, {polling: true});
+   
 
-    bot.setWebHook(CONSTS.Config.get('url') + "/bot" + token);
-    
-    bot.on('message', msg => {
-        
-        res = TBot.Bot.InputRes(msg)
+    bot.onText(/(.+)/, (msg, match) => {
+          
+        var res = TBot.Bot.InputRes(msg)
         
         console.log(res);
         bot.sendMessage(res.fromId, res.respoynse);
-        //console.log(res + "respounsed");
+        
     
     });
     
     return {
         Start: function() {
-           // console.log("Telegram Bot sacsesful started");
+           
         }
     }
 
